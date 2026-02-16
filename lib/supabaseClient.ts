43f919f8b5386a3a70ea2871cbfd28977699633a
Andreas Sedlag <DEINE_GITHUB_MAIL@...>
2026-02-16 11:32:04 +0100

    Fix supabase env fallback for Vercel

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  process.env.NEXTPUBLICSUPABASEURL ||
+  '';
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  process.env.NEXTPUBLICSUPABASEANONKEY ||
+  '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
